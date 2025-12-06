f3e98f21be7d43af2d2e7ff029d05346005afe2e
GrohJann <jannismaxim@gmail>
2025-12-06 02:58:29 +0100

    edited Networking code to not cause errors on linux

---
+++ src/Network/Client_Network.h
@@ -0,0 +1,13 @@
+/**
+ * @file Client_Network.h
+ * @brief TODO: Add a short description of what this file does.
+ *
+ * @author ruvaak
+ * @date 05/12/2025
+ */
+#ifndef QUANTUMCHESS_CLIENT_NETWORK_H
+#define QUANTUMCHESS_CLIENT_NETWORK_H
+
+int startClient();
+
+#endif //QUANTUMCHESS_CLIENT_NETWORK_H
